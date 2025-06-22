f92527f22e975bf9452b4d889a5d587fc2f094ec
Eduardo Kamo <eduardokamoz@gmail.com>
2025-06-22 17:46:31 -0300

    Update 20250423005848-create-table-user.js

---
+++ src/database/migrations/20250423005848-create-table-user.js
@@ -0,0 +1,46 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("user", {
+      id_user: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      score: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    })
+
+    // Criar índices
+    await queryInterface.addIndex("user", ["email"], {
+      unique: true,
+      name: "user_email_unique",
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("user")
+  },
+}
